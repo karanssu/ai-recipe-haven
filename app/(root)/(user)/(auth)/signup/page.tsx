c640711b5b653965707a905b8c56c2a52e32be42
karanssu <s0377009salemstate@gmail.com>
2024-12-17 11:27:20 -0500

    login with google btn added

--- app/(root)/(user)/(auth)/signup/page.tsx
+++ app/(root)/(user)/(auth)/signup/page.tsx
@@ -1,11 +1,23 @@
+import Form from "next/form";
+
 const Page = () => {
 	return (
 		<>
 			<div className="text-4xl text-center mt-10 font-semibold">Sign up</div>
 			<div className="flex justify-center mt-10">
-				<button className="bg-Green hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
-					Sign up
-				</button>
+				<Form
+					action={async () => {
+						"use server";
+						console.log("Sign up with Google");
+					}}
+				>
+					<button
+						type="submit"
+						className="bg-Green hover:bg-green-500 text-white font-bold py-2 px-4 rounded"
+					>
+						Log in with Google
+					</button>
+				</Form>
 			</div>
 		</>
 	);
