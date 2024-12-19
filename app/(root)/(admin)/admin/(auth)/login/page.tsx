667f27a378de2810ba30e2a4ce73d8d6c6616924
karanssu <s0377009salemstate@gmail.com>
2024-12-18 23:40:16 -0500

    Admin login page added

--- app/(root)/(admin)/admin/(auth)/login/page.tsx
+++ app/(root)/(admin)/admin/(auth)/login/page.tsx
@@ -1,6 +1,38 @@
+import Form from "next/form";
+
 const Page = () => {
 	return (
-		<div className="text-4xl text-center mt-10 font-semibold">Admin Log In</div>
+		<div className="border-2 border-gray-200 p-5 rounded-lg w-[400px] ml-auto mr-auto mt-10">
+			<div className="text-4xl text-center font-semibold">Admin Login</div>
+
+			<div className="mt-5">
+				<Form
+					action={async () => {
+						"use server";
+						console.log("Sign up with email and password");
+					}}
+				>
+					<input
+						type="text"
+						name="usernameEmail"
+						placeholder="Email or username"
+						className="border-2 border-gray-200 p-2 rounded-lg w-full mt-5"
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"
+						className="border-2 border-gray-200 p-2 rounded-lg w-full mt-5"
+					/>
+					<button
+						type="submit"
+						className="bg-Green hover:bg-green-500 text-white font-bold py-2 px-4 rounded w-full mt-5 mb-5"
+					>
+						Log in
+					</button>
+				</Form>
+			</div>
+		</div>
 	);
 };
 
