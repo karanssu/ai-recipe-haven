22f89ffdca62bfee303a4de9f2df568f06a96cbd
karanssu <s0377009salemstate@gmail.com>
2024-12-18 23:03:32 -0500

    basic login page styled

--- app/(root)/(user)/(auth)/login/page.tsx
+++ app/(root)/(user)/(auth)/login/page.tsx
@@ -1,5 +1,57 @@
+import Form from "next/form";
+
 const Page = () => {
-	return <div className="text-4xl text-center mt-10 font-semibold">Log in</div>;
+	return (
+		<div className="border-2 border-gray-200 p-5 rounded-lg w-[400px] ml-auto mr-auto mt-10">
+			<div className="text-4xl text-center font-semibold">Log in</div>
+
+			<div className="mt-5">
+				<Form
+					action={async () => {
+						"use server";
+						console.log("Sign up with email and password");
+					}}
+				>
+					<input
+						type="text"
+						name="usernameEmail"
+						placeholder="Email or username"
+						className="border-2 border-gray-200 p-2 rounded-lg w-full mt-5"
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"
+						className="border-2 border-gray-200 p-2 rounded-lg w-full mt-5"
+					/>
+					<button
+						type="submit"
+						className="bg-Green hover:bg-green-500 text-white font-bold py-2 px-4 rounded w-full mt-5"
+					>
+						Log in
+					</button>
+				</Form>
+			</div>
+
+			<hr className="mt-5" />
+
+			<div className="mt-5">
+				<Form
+					action={async () => {
+						"use server";
+						console.log("Sign up with Google");
+					}}
+				>
+					<button
+						type="submit"
+						className="bg-Green hover:bg-green-500 text-white font-bold py-2 px-4 w-full rounded"
+					>
+						Log in with Google
+					</button>
+				</Form>
+			</div>
+		</div>
+	);
 };
 
 export default Page;
