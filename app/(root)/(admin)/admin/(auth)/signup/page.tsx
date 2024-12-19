e0eaa7c24f6f36d3ebbc9941800d4ca42acf5511
karanssu <s0377009salemstate@gmail.com>
2024-12-18 23:43:29 -0500

    Admin sign up page added

--- app/(root)/(admin)/admin/(auth)/signup/page.tsx
+++ app/(root)/(admin)/admin/(auth)/signup/page.tsx
@@ -1,7 +1,56 @@
+import Form from "next/form";
+import Link from "next/link";
+
 const Page = () => {
 	return (
-		<div className="text-4xl text-center mt-10 font-semibold">
-			Admin Sign up
+		<div className="border-2 border-gray-200 p-5 rounded-lg w-[400px] ml-auto mr-auto mt-10">
+			<div className="text-4xl text-center font-semibold">Admin Sign up</div>
+
+			<div className="mt-5">
+				<Form
+					action={async () => {
+						"use server";
+						console.log("Sign up with email and password");
+					}}
+				>
+					<input
+						type="text"
+						name="name"
+						placeholder="Full Name"
+						className="border-2 border-gray-200 p-2 rounded-lg w-full mt-5"
+					/>
+					<input
+						type="text"
+						name="username"
+						placeholder="Username"
+						className="border-2 border-gray-200 p-2 rounded-lg w-full mt-5"
+					/>
+					<input
+						type="email"
+						name="email"
+						placeholder="Email"
+						className="border-2 border-gray-200 p-2 rounded-lg w-full mt-5"
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"
+						className="border-2 border-gray-200 p-2 rounded-lg w-full mt-5"
+					/>
+					<input
+						type="password"
+						name="confirmPassword"
+						placeholder="Confirm Password"
+						className="border-2 border-gray-200 p-2 rounded-lg w-full mt-5"
+					/>
+					<button
+						type="submit"
+						className="bg-Green hover:bg-green-500 text-white font-bold py-2 px-4 rounded w-full mt-5 mb-5"
+					>
+						Sign up
+					</button>
+				</Form>
+			</div>
 		</div>
 	);
 };
