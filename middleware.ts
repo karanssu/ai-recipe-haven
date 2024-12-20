12e3dac3497bfd73b7e0df814c186e6956ef3667
karanssu <s0377009salemstate@gmail.com>
2024-12-19 21:16:16 -0500

    middleware skip the logo.png

--- middleware.ts
+++ middleware.ts
@@ -36,6 +36,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
 	matcher: [
-		"/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+		"/((?!api|_next/static|_next/image|favicon.ico|logo.png|sitemap.xml|robots.txt).*)",
 	],
 };
