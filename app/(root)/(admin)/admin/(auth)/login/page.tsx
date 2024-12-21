138bee16d583dc00733be561ed94009e9f574d01
karanssu <s0377009salemstate@gmail.com>
2024-12-21 14:31:33 -0500

    signup and login page don't need to be async

--- app/(root)/(admin)/admin/(auth)/login/page.tsx
+++ app/(root)/(admin)/admin/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 import Form from "next/form";
 
-const Page = async () => {
+const Page = () => {
 	return (
 		<div className="border-2 border-gray-200 p-5 rounded-lg w-[400px] ml-auto mr-auto mt-10">
 			<div className="text-4xl text-center font-semibold">Admin Login</div>
--- app/(root)/(user)/(auth)/login/page.tsx
+++ app/(root)/(user)/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import { signIn } from "@/auth";
 import Form from "next/form";
 import Link from "next/link";
 
-const Page = async () => {
+const Page = () => {
 	return (
 		<div className="border-2 border-gray-200 p-5 rounded-lg w-[400px] ml-auto mr-auto mt-10">
 			<div className="text-4xl text-center font-semibold">Log in</div>
