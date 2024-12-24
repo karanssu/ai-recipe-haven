d02d4df0e34e1c1eb3b29aedbf4c2721310c415b
karanssu <s0377009salemstate@gmail.com>
2024-12-23 19:18:46 -0500

    mongoose connection implementation changed

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -1,10 +1,18 @@
-import mongoose from "mongoose";
+import { connect } from "mongoose";
 
 export const connectMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGODB_URI as string);
+		const { connection } = await connect(process.env.MONGODB_URI as string);
+		if (connection.readyState === 1) {
+			console.log("Connected to MongoDB");
+			return Promise.resolve(true);
+		}
+
 		console.log("Connected to MongoDB");
 	} catch (error) {
+		console.error(error);
 		console.error("Error connecting to MongoDB", error);
+		return Promise.reject(error);
 	}
 };
+export const connectDB = async () => {};
