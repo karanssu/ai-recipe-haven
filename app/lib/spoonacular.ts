cd2da6bdf3bfe4ecdf54b23d73cc1717633a1c27
karanssu <s0377009salemstate@gmail.com>
2025-01-02 13:10:46 -0500

    Code refactored

--- app/lib/spoonacular.ts
+++ app/lib/spoonacular.ts
@@ -1,16 +1,8 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-// https://api.spoonacular.com/recipes/complexSearch?addRecipeInstructions=true&addRecipeNutrition=true&sortDirection=desc&offset=0&number=20
 
-// I want to create a function that will create data.json file if it doesn't exist
-// and if it does exist, it will append the data to the file
-// the format is as follows:
-// {
-//   "recipes": [] <- this is an array of recipes where each recipe is an object here will the data be appended
-// }
-// function will be called saveRecipes
-// it will take in an array of recipes
+// https://api.spoonacular.com/recipes/complexSearch?addRecipeInstructions=true&addRecipeNutrition=true&sortDirection=desc&offset=0&number=20
 
 export const saveRecipes = (recipes: object[]) => {
 	const dirPath = path.dirname(fileURLToPath(import.meta.url));
