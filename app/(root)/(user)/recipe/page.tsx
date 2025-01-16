85a4f4850b02ed76f7ab0bb5b3351f541cc9f277
karanssu <s0377009salemstate@gmail.com>
2025-01-16 09:15:56 -0500

    Code refactored

--- app/(root)/(user)/recipe/page.tsx
+++ app/(root)/(user)/recipe/page.tsx
@@ -1,6 +1,5 @@
 import RecipeCard from "@/app/components/(user)/RecipeCard";
 import { RecipeCardDef } from "@/app/lib/definitions";
-import Link from "next/link";
 
 const getRecipes = async () => {
 	"use server";
@@ -26,24 +25,6 @@ const Page = async () => {
 			<div className="text-4xl text-center mt-10 font-semibold font-title">
 				All Recipes
 			</div>
-
-			<ul className="mt-10 text-center text-xl cursor-pointer space-y-5 text-primaryText">
-				<li>
-					<Link className="underline" href={`/recipe/1`}>
-						Recipe 1
-					</Link>
-				</li>
-				<li>
-					<Link className="underline" href={`/recipe/2`}>
-						Recipe 2
-					</Link>
-				</li>
-				<li>
-					<Link className="underline" href={`/recipe/3`}>
-						Recipe 3
-					</Link>
-				</li>
-			</ul>
 
 			<div className="mt-10 px-10 py-5 flex overflow-x-auto space-x-10">
 				{recipes.map((recipe) => (
