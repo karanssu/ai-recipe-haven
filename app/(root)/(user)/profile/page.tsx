d56b5e7f0a0af3dffe56c3b514f7d0d88d0d6716
karanssu <s0377009salemstate@gmail.com>
2025-02-21 14:28:39 -0500

    EditProfile with mock user added

--- app/(root)/(user)/profile/page.tsx
+++ app/(root)/(user)/profile/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import EditUserForm from "@/app/components/(admin)/EditUserForm";
+import { User } from "@/app/lib/definitions";
+
 const Page = () => {
+	const user: User = {
+		_id: 1,
+		name: "Test",
+		role: "admin",
+		username: "test",
+		password: "test",
+		profileImage: "https://www.google.com",
+		email: "test@gmail.com",
+	};
+
 	return (
-		<div className="text-4xl text-center mt-10 font-semibold font-title">
-			My Profile
+		<div className="border-2 border-gray-200 p-5 rounded-lg w-[500px] ml-auto mr-auto mt-10">
+			<EditUserForm
+				title="Profile"
+				user={user}
+				revalidatePageAction={() => {
+					console.log("user profile edited");
+				}}
+			/>
 		</div>
 	);
 };
