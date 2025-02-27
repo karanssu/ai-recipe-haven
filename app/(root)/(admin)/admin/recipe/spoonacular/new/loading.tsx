e5088c31b125214b09ed22ae9fb95e26ca0351a4
karanssu <s0377009salemstate@gmail.com>
2025-02-27 13:50:39 -0500

    Loading gif added while fetching Recipes

---
+++ app/(root)/(admin)/admin/recipe/spoonacular/new/loading.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+const Page = () => {
+	return (
+		<>
+			<div className="font-title font-semibold text-center mt-10 text-3xl">
+				<div>Fetching New Recipes...</div>
+				<div>
+					<Image
+						src="/loading.gif"
+						width={50}
+						height={50}
+						alt="Loading..."
+						className="mx-auto mt-4"
+					/>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Page;
