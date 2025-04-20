dd22978373976f0d4e673616ebef70166f00a429
karanssu <s0377009salemstate@gmail.com>
2025-04-20 12:03:54 -0400

    Admin Home Page manage recipes

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+const Page = () => {
+	return (
+		<>
+			<div className="font-title font-semibold text-center mt-10 text-3xl">
+				<div>
+					<Image
+						src="/loading.gif"
+						width={50}
+						height={50}
+						alt="Loading..."
+						className="mx-auto mt-4"
+					/>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Page;
