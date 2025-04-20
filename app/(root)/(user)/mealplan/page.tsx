bbd71670765fc897cf6a9cb58da75f8b74586557
karanssu <s0377009salemstate@gmail.com>
2025-04-20 11:11:44 -0400

    Meal Plan page removed

--- app/(root)/(user)/mealplan/page.tsx
+++
@@ -1,9 +0,0 @@
-const Page = () => {
-	return (
-		<div className="text-4xl text-center mt-10 font-semibold font-title">
-			Meal Plan
-		</div>
-	);
-};
-
-export default Page;
--- app/components/(user)/Navbar.tsx
+++ app/components/(user)/Navbar.tsx
@@ -25,8 +25,7 @@ const Navbar = async () => {
 					<>
 						<div>Hello, {user?.name as string}</div>
 						<Link href="/recipe">All Recipes</Link>
-						<Link href="/recipe/new">Create Recipes</Link>
-						<Link href="/mealplan">Meal Plan</Link>
+						<Link href="/recipe/new">Create Recipe</Link>
 						<Link href="/profile">Profile</Link>
 						<form
 							action={async () => {
